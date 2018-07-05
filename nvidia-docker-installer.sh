82943d468651aade06489845d29ec76e9e5fde52
ducta-qc <duc@chappiebot.com>
2018-07-05 19:39:50 +0700

    Update nvidia-docker-installer.sh

--- nvidia-docker-installer.sh
+++ nvidia-docker-installer.sh
@@ -5,73 +5,53 @@ set -o pipefail
 set -u
 
 set -x
-NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-384.125}"
-NVIDIA_DRIVER_DOWNLOAD_URL_DEFAULT="https://us.download.nvidia.com/tesla/${NVIDIA_DRIVER_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_DRIVER_VERSION}.run"
-NVIDIA_DRIVER_DOWNLOAD_URL="${NVIDIA_DRIVER_DOWNLOAD_URL:-$NVIDIA_DRIVER_DOWNLOAD_URL_DEFAULT}"
-NVIDIA_INSTALLER_RUNFILE="$(basename "${NVIDIA_DRIVER_DOWNLOAD_URL}")"
-NVIDIA_INSTALL_DIR="${NVIDIA_INSTALL_DIR:-/tmp}"
-set -x
 
+apt-get update
+apt-get install --yes gcc
 
-RETCODE_SUCCESS=0
-RETCODE_ERROR=1
-RETRY_COUNT=5
+cd /tmp
+wget https://s3.amazonaws.com/chappiebot/packages/cuda/NVIDIA-Linux-x86_64-390.12.run
+chmod +x NVIDIA-Linux-x86_64-390.12.run
+/tmp/NVIDIA-Linux-x86_64-390.12.run --accept-license --ui=none
 
+nvidia-smi -pm 1
+nvidia-smi -acp 0
+nvidia-smi --auto-boost-default=0
+nvidia-smi --auto-boost-permission=0
+nvidia-smi -ac 2505,875
 
-download_kernel_src() {
-  echo "Downloading kernel sources..."
-  apt-get update
-  apt-get install -y linux-headers-$(uname -r)
-  apt-get install -y gcc libc-dev
-  echo "Downloading kernel sources... DONE."
-}
+/sbin/modprobe nvidia-uvm
 
-download_nvidia_installer() {
-  echo "Downloading Nvidia installer..."
-  pushd "${NVIDIA_INSTALL_DIR}"
-  curl -L -S -f "${NVIDIA_DRIVER_DOWNLOAD_URL}" -o "${NVIDIA_INSTALLER_RUNFILE}"
-  popd
-  echo "Downloading Nvidia installer... DONE."
-}
+if [ "$?" -eq 0 ]; then
+  # Find out the major device number used by the nvidia-uvm driver
+  D=`grep nvidia-uvm /proc/devices | awk '{print $1}'`
+  mknod -m 666 /dev/nvidia-uvm c $D 0
+else
+  echo "Unable to modprobe nvidia-uvm"
+fi
 
-run_nvidia_installer() {
-  echo "Running Nvidia installer..."
-  pushd "${NVIDIA_INSTALL_DIR}"
-  sh "${NVIDIA_INSTALLER_RUNFILE}" \
-    --no-install-compat32-libs \
-    --log-file-name="${NVIDIA_INSTALL_DIR}/nvidia-installer.log" \
-    --no-drm \
-    --silent \
-    --accept-license
-  popd
-  echo "Running Nvidia installer... DONE."
-}
+# Install docker-ce, nvidia-docker2
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
-configure_gpu() {
-  nvidia-smi -pm 1
-  nvidia-smi -acp 0
-  nvidia-smi --auto-boost-default=0
-  nvidia-smi --auto-boost-permission=0
-  nvidia-smi -ac 2505,875
-}
+# Add nvidia docker repo
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/debian8/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
 
-verify_nvidia_installation() {
-  echo "Verifying Nvidia installation..."
-  nvidia-smi
-  nvidia-modprobe -c0 -u
-  echo "Verifying Nvidia installation... DONE."
-}
+# Add docker-ce repo 
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
-install_nvidia_docker2() {
-  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
-  apt-get update
-  apt-get install -y nvidia-docker2=2.0.3+docker17.03.2-1 nvidia-container-runtime=2.0.0+docker17.03.2-1
-}
+# Get docker-ce, nvidia-docker2
+apt-get update
+sed -i 's/^ExecStart=\/usr\/bin\/dockerd -H fd:\/\/.*$/ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ -s=overlay2/' /lib/systemd/system/docker.service
+apt-get install -y docker-ce 
+apt-get install -y nvidia-docker2
 
-set_nvidia_container_runtime() {
-  cat > /etc/docker/daemon.json <<EOL
+systemctl daemon-reload
+apt-get install -y nvidia-docker2
+tee /etc/docker/daemon.json <<EOF
 {
     "default-runtime": "nvidia",
     "runtimes": {
@@ -81,17 +61,5 @@ set_nvidia_container_runtime() {
         }
     }
 }
-EOL
-}
-
-main() {
-  download_kernel_src
-  download_nvidia_installer
-  run_nvidia_installer
-  configure_gpu
-  verify_nvidia_installation
-  install_nvidia_docker2
-  set_nvidia_container_runtime
-}
-
-main "$@"
+EOF
+pkill -SIGHUP dockerd
